ab8ca8916a22a58c21cc98ac441aacc5214c426d
Alexandru Stanica <61120305+AlexandruStanica@users.noreply.github.com>
2022-01-19 16:06:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+        "<i class='fas fa-chevron-left'></i>",
+        "<i class='fas fa-chevron-right'></i>"
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+flatpickr("#date", {
+    minDate: "today"
+});
